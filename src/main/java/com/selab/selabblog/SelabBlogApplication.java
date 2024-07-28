1c3117b5acc5d2637b7b9015fa55e54104bcfbd8
GALAwang <3221134362@qq.com>
2024-07-28 19:44:11 +0800

    初步框架

---
+++ src/main/java/com/selab/selabblog/SelabBlogApplication.java
@@ -0,0 +1,13 @@
+package com.selab.selabblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelabBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelabBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/selab/selabblog/SelabBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selab.selabblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelabBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
