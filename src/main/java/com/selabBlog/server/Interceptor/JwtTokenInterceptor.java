0d4859ab7ad0c2b22fb771bff2006cb87ae466a3
junyue <982293013@qq.com>
2024-08-09 21:46:31 +0800

    删除分页功能相关代码

--- src/main/java/com/selabBlog/server/Interceptor/JwtTokenInterceptor.java
+++ src/main/java/com/selabBlog/server/Interceptor/JwtTokenInterceptor.java
@@ -1,45 +1,47 @@
 package com.selabBlog.server.Interceptor;
-
-import com.selabBlog.common.context.BaseContext;
-import com.selabBlog.common.properties.JwtProperties;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.HandlerInterceptor;
-/**
- * jwt令牌校验的拦截器
- */
-@Component
-@Slf4j
-public class JwtTokenInterceptor implements HandlerInterceptor {
-
-    @Autowired
-    private JwtProperties jwtProperties;
-
-    /**
-     * 校验jwt
-     *
-     * @param request
-     * @param response
-     * @param handler
-     * @return
-     * @throws Exception
-     */
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //判断当前拦截到的是Controller的方法还是其他资源
-        if (!(handler instanceof HandlerMethod)) {
-            //当前拦截到的不是动态方法，直接放行
-            return true;
-        }
-
-        //1、从请求头中获取令牌
-        String token = request.getHeader(jwtProperties.getTokenName());
-
-        //2、校验令牌
-        //TODO token拿不到，我把userid值默认设为了1   整合时要修改
+//
+//import com.selabBlog.common.Utils.JwtUtil;
+//import com.selabBlog.common.context.BaseContext;
+//import com.selabBlog.common.properties.JwtProperties;
+//
+//import io.jsonwebtoken.Claims;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.method.HandlerMethod;
+//import org.springframework.web.servlet.HandlerInterceptor;
+///**
+// * jwt令牌校验的拦截器
+// */
+//@Component
+//@Slf4j
+//public class JwtTokenInterceptor implements HandlerInterceptor {
+//
+//    @Autowired
+//    private JwtProperties jwtProperties;
+//
+//    /**
+//     * 校验jwt
+//     *
+//     * @param request
+//     * @param response
+//     * @param handler
+//     * @return
+//     * @throws Exception
+//     */
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        //判断当前拦截到的是Controller的方法还是其他资源
+//        if (!(handler instanceof HandlerMethod)) {
+//            //当前拦截到的不是动态方法，直接放行
+//            return true;
+//        }
+//
+//        //1、从请求头中获取令牌
+//        String token = request.getHeader(jwtProperties.getTokenName());
+//
+//        //2、校验令牌
 //        try {
 //            log.info("jwt校验:{}", token);
 //            Claims claims = JwtUtil.parseJWT(jwtProperties.getSecretKey(), token);
@@ -53,7 +55,5 @@ public class JwtTokenInterceptor implements HandlerInterceptor {
 //            response.setStatus(401);
 //            return false;
 //        }
-        BaseContext.setCurrentId(3L);
-        return true;
-    }
-}
+//    }
+//}
