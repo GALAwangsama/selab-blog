6d9f913d32484ba7a2fe563db3c588354bbafb1a
Susie <3312321174@qq.com>
2024-08-14 15:50:05 +0800

    feat:增删改选文章的四个接口

--- src/main/java/com/selabBlog/server/Controller/ArticleController.java
+++ src/main/java/com/selabBlog/server/Controller/ArticleController.java
@@ -1,12 +1,16 @@
 package com.selabBlog.server.Controller;
 
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import com.selabBlog.common.context.BaseContext;
 import com.selabBlog.pojo.DTO.SaveArticleDTO;
 import com.selabBlog.pojo.DTO.UpdateDTO;
 import com.selabBlog.pojo.Result.Result;
 import com.selabBlog.pojo.VO.SelectAllVO;
 import com.selabBlog.pojo.VO.SelectArticleVO;
 import com.selabBlog.pojo.VO.SelectVO;
+import com.selabBlog.pojo.entity.Article;
 import com.selabBlog.server.Service.ArticleService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -42,25 +46,53 @@ public class ArticleController {
     //TODO 新增文章
     @PostMapping("/saveArticle")
     public Result save(@RequestBody SaveArticleDTO saveArticleDTO){
-        return null;
+        if(saveArticleDTO.getTitle() == null || saveArticleDTO.getContent() == null
+                || saveArticleDTO.getImage() == null){
+            return Result.error("信息填写不完整");
+        }
+        Article article = BeanUtil.copyProperties(saveArticleDTO, Article.class);
+        article.setPublishTime(DateTime.now().toLocalDateTime());
+        BaseContext baseContext = new BaseContext();
+        Long userId = baseContext.getCurrentId();
+        article.setUserId(userId);
+        boolean flag = articleService.save(article);
+        if (flag){
+            return Result.success();
+        }else {
+            return Result.error("文章新增失败");
+        }
     }
 
     //TODO 选择文章
     @GetMapping("selectByArticleId/{id}")
-    public Result<SelectArticleVO> selectByArticle(@PathVariable Long id){
-        return null;
+    public Result<SelectArticleVO> selectByArticleId(@PathVariable Long id){
+        Result result = articleService.selectByArticleId(id);
+        return result;
     }
 
     //TODO 删除文章
     @DeleteMapping("/deleteArticle/{id}")
     public Result delete(@PathVariable Long id){
-        return null;
+        boolean flag = articleService.removeById(id);
+        if(flag){
+            return Result.success();
+        }else{
+            return Result.error("文章删除失败");
+        }
     }
 
     //TODO 修改文章
     @PutMapping("/updateArticle")
     public Result update(@RequestBody UpdateDTO updateDTO){
-        return null;
+        Article article = BeanUtil.copyProperties(updateDTO, Article.class);
+        article.setUpdateTime(DateTime.now().toLocalDateTime());
+        article.setArticleId(updateDTO.getId());
+        boolean flag = articleService.updateById(article);
+        if(flag){
+            return Result.success();
+        }else{
+            return Result.error("文章修改失败");
+        }
     }
 
     //TODO 查询文章详情
